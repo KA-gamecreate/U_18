d3fb00f6afbea5db211e6d34ae3e0f4142b442a0
”KA-gamecreate” <burakki.efi.22@docomo.ne.jp>
2021-03-19 23:26:34 +0900

    fungus入れた

--- Assets/Scripts/skill.cs
+++ Assets/Scripts/skill.cs
@@ -7,10 +7,13 @@ public class skill : MonoBehaviour
 {
     public GameObject pl1;
     public GameObject skill2;
+    public GameObject skill3;
     public GameObject pl2;
     public GameObject en2;
     public GameObject button1;
     public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
     StageScript script;
     GameObject stagemaker;
     AudioSource audioSource;
@@ -28,7 +31,26 @@ public class skill : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(stagemaker.GetComponent<StageScript>().playerTurn == true)
+        {
+            button4.SetActive(false);
+            button3.SetActive(true);
+        }
+        if (stagemaker.GetComponent<StageScript>().playerTurn == false)
+        {
+            button3.SetActive(false);
+            button4.SetActive(true);
+        }
+        if (stagemaker.GetComponent<StageScript>().enemy.activeSelf == false)
+        {
+            button4.SetActive(false);
+            Debug.Log("は？");
+        }
+        if (stagemaker.GetComponent<StageScript>().player.activeSelf == false)
+        {
+            button3.SetActive(false);
+            Debug.Log("お？");
+        }
     }
     public void NoplSkill()
     {
@@ -57,5 +79,21 @@ public class skill : MonoBehaviour
         button1.SetActive(false);
         audioSource.PlayOneShot(sound);
     }
-
+    public void Yasumitwo()
+    {
+            button4.SetActive(true);
+            button3.SetActive(false);
+            stagemaker.GetComponent<StageScript>().playerTurn = false;
+            audioSource = GetComponent<AudioSource>();
+            audioSource.PlayOneShot(sound);
+        
+    }
+    public void Yasumithree()
+    {
+        button3.SetActive(true);
+        button4.SetActive(false);
+        stagemaker.GetComponent<StageScript>().playerTurn = true;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(sound);
+    }
 }

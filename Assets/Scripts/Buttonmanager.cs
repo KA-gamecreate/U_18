3d4c8eb919599cf9685312fb0731af71de3a5fa4
”KA-gamecreate” <burakki.efi.22@docomo.ne.jp>
2021-03-27 16:17:30 +0900

    うっせえわ

---
+++ Assets/Scripts/Buttonmanager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttonmanager : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void Tutorial()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(sound);
+        Invoke("Tutorialidou", 1.0f);
+
+    }
+    public void Shop()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(sound);
+        Invoke("shopidou",1.0f);
+       
+    }
+    public void Shopidou()
+    {
+        SceneManager.LoadScene("shop");
+    }
+    public void Tutorialidou()
+    {
+        SceneManager.LoadScene("tutorial");
+    }
+}
